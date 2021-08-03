5a65416da7ecb7b50291c4a017c665852f559bf4
PHildreth <phildreth@gmail.com>
2021-08-02 22:26:30 -0400

    UI fix for 5.7 skill trees

--- TT2Advisor.UI/ClientApp/src/components/myBuild/skillTree.js
+++ TT2Advisor.UI/ClientApp/src/components/myBuild/skillTree.js
@@ -42,18 +42,57 @@ export class SkillTree extends Component {
                                                         </tr>
                                                         <tr>
                                                             <td><Skill skill={this.state.skillTree.chivalricOrder} /></td>
-                                                            <td><Skill skill={this.state.skillTree.petEvolution} /></td>
-                                                            <td><Skill skill={this.state.skillTree.heartofMidas} /></td>
+                                                            <td><Skill skill={this.state.skillTree.angelicRadiance} /></td>
+                                                            <td><Skill skill={this.state.skillTree.midasUltimate} /></td>
                                                         </tr>
                                                         <tr>
                                                             <td><Skill skill={this.state.skillTree.cleavingStrike} /></td>
-                                                            <td><Skill skill={this.state.skillTree.summonInferno} /></td>
-                                                            <td><Skill skill={this.state.skillTree.lightningBurst} /></td>
+                                                            <td><Skill skill={this.state.skillTree.rejuvenation} /></td>
+                                                            <td><Skill skill={this.state.skillTree.willOfMidas} /></td>
                                                         </tr>
                                                         <tr>
                                                             <td><Skill skill={this.state.skillTree.barbaricFury} /></td>
-                                                            <td><Skill skill={this.state.skillTree.volcanicSupremacy} /></td>
+                                                            <td><Skill skill={this.state.skillTree.divineWrath} /></td>
+                                                            <td><Skill skill={this.state.skillTree.fairyCharm} /></td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                            <td>
+                                <table className='table table-striped' aria-labelledby="tabelLabel">
+                                    <thead>
+                                        <tr>
+                                            <th>Summoner Tree</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <table>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td />
+                                                            <td><Skill class='knightTree' skill={this.state.skillTree.petEvolution} /></td>
+                                                            <td />
+                                                        </tr>
+                                                        <tr>
+                                                            <td><Skill skill={this.state.skillTree.summonInferno} /></td>
+                                                            <td><Skill skill={this.state.skillTree.heartofMidas} /></td>
+                                                            <td><Skill skill={this.state.skillTree.companionWarfare} /></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td><Skill skill={this.state.skillTree.emberArts} /></td>
+                                                            <td><Skill skill={this.state.skillTree.lightningBurst} /></td>
+                                                            <td><Skill skill={this.state.skillTree.summoningCircle} /></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td><Skill skill={this.state.skillTree.volcanicEruption} /></td>
                                                             <td><Skill skill={this.state.skillTree.flashZip} /></td>
+                                                            <td><Skill skill={this.state.skillTree.burningPassion} /></td>
                                                         </tr>
                                                     </tbody>
                                                 </table>
@@ -102,91 +141,86 @@ export class SkillTree extends Component {
                                 </table>
                             </td>
                         </tr>
-                        {/*<tr>*/}
-                        {/*    <td>*/}
-                        {/*        <table className='table table-striped' aria-labelledby="tabelLabel">*/}
-                        {/*            <thead>*/}
-                        {/*                <tr>*/}
-                        {/*                    <th>Sorcerer Tree</th>*/}
-                        {/*                </tr>*/}
-                        {/*            </thead>*/}
-                        {/*            <tbody>*/}
-                        {/*                <tr>*/}
-                        {/*                    <td>*/}
-                        {/*                        <table>*/}
-                        {/*                            <tbody>*/}
-                        {/*                                <tr>*/}
-                        {/*                                    <td />*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.limitBreak} /></td>*/}
-                        {/*                                    <td />*/}
-                        {/*                                </tr>*/}
-                        {/*                                <tr>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.midasUltimate} /></td>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.angelicRadiance} /></td>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.phantomVengeance} /></td>*/}
-                        {/*                                </tr>*/}
-                        {/*                                <tr>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.fairyCharm} /></td>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.manaSiphon} /></td>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.eternalDarkness} /></td>*/}
-                        {/*                                </tr>*/}
-                        {/*                                <tr>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.manniMana} /></td>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.lightningStrike} /></td>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.dimensionalShift} /></td>*/}
-                        {/*                                </tr>*/}
-                        {/*                                <tr>*/}
-                        {/*                                    <td />*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.divineSupremacy} /></td>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.phantomSupremacy} /></td>*/}
-                        {/*                                </tr>*/}
-                        {/*                            </tbody>*/}
-                        {/*                        </table>*/}
-                        {/*                    </td>*/}
-                        {/*                </tr>*/}
-                        {/*            </tbody>*/}
-                        {/*        </table>*/}
-                        {/*    </td>*/}
-                        {/*    <td>*/}
-                        {/*        <table className='table table-striped' aria-labelledby="tabelLabel">*/}
-                        {/*            <thead>*/}
-                        {/*                <tr>*/}
-                        {/*                    <th>Rogue Tree</th>*/}
-                        {/*                </tr>*/}
-                        {/*            </thead>*/}
-                        {/*            <tbody>*/}
-                        {/*                <tr>*/}
-                        {/*                    <td>*/}
-                        {/*                        <table>*/}
-                        {/*                            <tbody>*/}
-                        {/*                                <tr>*/}
-                        {/*                                    <td />*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.masterThief} /></td>*/}
-                        {/*                                    <td />*/}
-                        {/*                                </tr>*/}
-                        {/*                                <tr>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.ambush} /></td>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.assassinate} /></td>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.summonDagger} /></td>*/}
-                        {/*                                </tr>*/}
-                        {/*                                <tr>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.strokeOfLuck} /></td>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.daggerStorm} /></td>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.cloaking} /></td>*/}
-                        {/*                                </tr>*/}
-                        {/*                                <tr>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.forbiddenContract} /></td>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.poisonEdge} /></td>*/}
-                        {/*                                    <td><Skill skill={this.state.skillTree.deadlyFocus} /></td>*/}
-                        {/*                                </tr>*/}
-                        {/*                            </tbody>*/}
-                        {/*                        </table>*/}
-                        {/*                    </td>*/}
-                        {/*                </tr>*/}
-                        {/*            </tbody>*/}
-                        {/*        </table>*/}
-                        {/*    </td>*/}
-                        {/*</tr>*/}
+                        <tr>
+                            <td>
+                                <table className='table table-striped' aria-labelledby="tabelLabel">
+                                    <thead>
+                                        <tr>
+                                            <th>Sorcerer Tree</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <table>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td />
+                                                            <td><Skill skill={this.state.skillTree.phantomVengeance} /></td>
+                                                            <td />
+                                                        </tr>
+                                                        <tr>
+                                                            <td><Skill skill={this.state.skillTree.limitBreak} /></td>
+                                                            <td><Skill skill={this.state.skillTree.eternalDarkness} /></td>
+                                                            <td><Skill skill={this.state.skillTree.manaSiphon} /></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td><Skill skill={this.state.skillTree.manniMana} /></td>
+                                                            <td><Skill skill={this.state.skillTree.dimensionalShift} /></td>
+                                                            <td><Skill skill={this.state.skillTree.lightningStrike} /></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td><Skill skill={this.state.skillTree.forbiddenContract} /></td>
+                                                            <td />
+                                                            <td />
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                            <td>
+                                <table className='table table-striped' aria-labelledby="tabelLabel">
+                                    <thead>
+                                        <tr>
+                                            <th>Rogue Tree</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <table>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td />
+                                                            <td><Skill skill={this.state.skillTree.masterThief} /></td>
+                                                            <td />
+                                                        </tr>
+                                                        <tr>
+                                                            <td><Skill skill={this.state.skillTree.ambush} /></td>
+                                                            <td><Skill skill={this.state.skillTree.assassinate} /></td>
+                                                            <td><Skill skill={this.state.skillTree.summonDagger} /></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td><Skill skill={this.state.skillTree.strokeOfLuck} /></td>
+                                                            <td><Skill skill={this.state.skillTree.daggerStorm} /></td>
+                                                            <td><Skill skill={this.state.skillTree.cloaking} /></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td />
+                                                            <td><Skill skill={this.state.skillTree.poisonEdge} /></td>
+                                                            <td><Skill skill={this.state.skillTree.deadlyFocus} /></td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
